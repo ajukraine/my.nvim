30f93784b26f6523df66fd687c67e8350e417314
Bohdan Makohin <ajukraine@gmail.com>
2022-02-09 23:20:24 +0200

    Set transparent background for 'material-gruvbox'

--- lua/config.lua
+++ lua/config.lua
@@ -15,6 +15,7 @@ require('packer').startup(function ()
       vim.g.gruvbox_material_better_performance = 1
 
       vim.g.gruvbox_italics = 0
+      vim.g.gruvbox_material_transparent_background = 1
       vim.g.gruvbox_material_background = 'hard'
       vim.g.gruvbox_material_enable_italic = 0
     end
