2dcfcee9c4da01a9f9c50f1c1a51041df7a4732c
Bohdan Makohin <ajukraine@gmail.com>
2022-03-18 02:14:38 +0200

    Add plugin 'nvim-lsp-installer'

--- lua/config.lua
+++ lua/config.lua
@@ -58,11 +58,14 @@ require('packer').startup(function ()
   }
 
   use 'nvim-treesitter/nvim-treesitter'--[[ , {'do': ':TSUpdate'} ]]
+
+  use 'neovim/nvim-lspconfig'
+  use 'williamboman/nvim-lsp-installer'
+
   -- use 'folke/twilight.nvim'
   -- use 'nvim-treesitter/nvim-treesitter-textobjects'
   -- use 'nvim-treesitter/nvim-treesitter-refactor'
   --
-  -- use 'neovim/nvim-lspconfig'
   -- use 'hrsh7th/nvim-cmp'
   -- use 'hrsh7th/cmp-nvim-lsp'
   -- use 'onsails/lspkind-nvim'
@@ -224,6 +227,25 @@ require('gitsigns').setup {
 --   }
 -- }
 --
+
+local lsp_installer = require("nvim-lsp-installer")
+
+-- Register a handler that will be called for each installed server when it's ready (i.e. when installation is finished
+-- or if the server is already installed).
+lsp_installer.on_server_ready(function(server)
+    local opts = {}
+
+    -- (optional) Customize the options passed to the server
+    -- if server.name == "tsserver" then
+    --     opts.root_dir = function() ... end
+    -- end
+
+    -- This setup() function will take the provided server configuration and decorate it with the necessary properties
+    -- before passing it onwards to lspconfig.
+    -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+    server:setup(opts)
+end)
+
 -- -- Add additional capabilities supported by nvim-cmp
 -- local cmp_capabilities = vim.lsp.protocol.make_client_capabilities()
 -- cmp_capabilities = require('cmp_nvim_lsp')
