dfd3e6362d62e6b4a81e800ec5d5861da5c79cd2
Bohdan Makohin <ajukraine@gmail.com>
2022-03-19 02:57:39 +0200

    Create separate util for <Cmd> mapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -2,21 +2,25 @@ local utils = require('utils')
 local nnoremap = utils.nnoremap
 local map = utils.map
 
+local function cmd(shortcut, command)
+  return nnoremap(shortcut, ':' .. command .. '<CR>')
+end
+
 vim.g.mapleader = ' '
 
-nnoremap('<leader>w',  ':w<CR>')
-nnoremap('<leader>q',  ':q<CR>')
-nnoremap('<leader>d',  ':bp|bd #<CR>')
-nnoremap('<leader>z',  ':Twilight<CR>')
-nnoremap('<leader>fb', ':Telescope buffers<cr>')
-nnoremap('<leader>fs', ':Telescope current_buffer_fuzzy_find<cr>')
-nnoremap('<leader>ff', ':Telescope find_files<cr>')
-nnoremap('<leader>g',  ':Git<cr>')
-nnoremap('<leader>e',  ':NvimTreeToggle<CR>')
+cmd('<leader>w',  'w')
+cmd('<leader>q',  'q')
+cmd('<leader>d',  'bp|bd #')
+cmd('<leader>z',  'Twilight')
+cmd('<leader>fb', 'Telescope buffers')
+cmd('<leader>fs', 'Telescope current_buffer_fuzzy_find')
+cmd('<leader>ff', 'Telescope find_files')
+cmd('<leader>g',  'Git')
+cmd('<leader>e',  'NvimTreeToggle')
 
-nnoremap('<Tab>',      ':bnext<CR>')
-nnoremap('<S-Tab>',    ':bprev<CR>')
+cmd('<Tab>',      'bnext')
+cmd('<S-Tab>',    'bprev')
 
-nnoremap('<leader>gb', ':Gitsigns toggle_current_line_blame<CR>')
+cmd('<leader>gb', 'Gitsigns toggle_current_line_blame')
 
 map({ 'x', 'n' }, 'ga', '<Plug>(EasyAlign)')
