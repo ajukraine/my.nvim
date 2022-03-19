2b065797d3b4db70049fa3d2a3f9237da2427ddc
Bohdan Makohin <ajukraine@gmail.com>
2022-03-19 16:03:32 +0200

    Configure Telescope mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,9 +8,6 @@ cmd('<leader>w',  'w')
 cmd('<leader>q',  'q')
 cmd('<leader>d',  'bp|bd #')
 cmd('<leader>z',  'Twilight')
-cmd('<leader>fb', 'Telescope buffers')
-cmd('<leader>fs', 'Telescope current_buffer_fuzzy_find')
-cmd('<leader>ff', 'Telescope find_files')
 cmd('<leader>g',  'Git')
 cmd('<leader>e',  'NvimTreeToggle')
 
@@ -19,8 +16,22 @@ cmd('<S-Tab>',    'bprev')
 
 cmd('<leader>gb', 'Gitsigns toggle_current_line_blame')
 
-cmd('<leader><leader>b', 'Telescope buffers')
-cmd('<leader><leader>ls', "lua require'telescope.builtin'.lsp_dynamic_workspace_symbols{}")
-cmd('<leader><leader>lr', "lua require'telescope.builtin'.lsp_references{}")
+-- Telescope
+local telescope_prefix = '<leader><leader>'
+local telescope_mappings = {
+  ['b']  = 'buffers',
+  ['f']  = 'find_files',
+  ['g']  = 'git_files',
+  ['s']  = 'current_buffer_fuzzy_find',
+  ['ls'] = 'lsp_dynamic_workspace_symbols',
+  ['lr'] = 'lsp_references',
+  ['li'] = 'lsp_implementations',
+  ['la'] = 'lsp_code_actions',
+}
+
+for key, picker in pairs(telescope_mappings) do
+  cmd(telescope_prefix .. key, "lua require'telescope.builtin'." .. picker .. "{}")
+end
+
 
 map({ 'x', 'n' }, 'ga', '<Plug>(EasyAlign)')
