e0222e102b1afed62b29fff5503e802d921deaac
Bohdan Makohin <ajukraine@gmail.com>
2022-03-30 22:59:42 +0300

    Add global inspection function P

--- init.lua
+++ init.lua
@@ -5,6 +5,11 @@ if DEBUG then
   vim.cmd [[set rtp?]]
 end
 
+function _G.P(v)
+  print(vim.inspect(v))
+  return v
+end
+
 local ok, impatient = pcall(require, 'impatient')
 if ok and DEBUG then
   impatient.enable_profile()
