04a7cb059feb2ecec114f65b22c96650eb3a04c8
Bohdan Makohin <ajukraine@gmail.com>
2022-04-04 08:05:36 +0300

    (omnisharp) Integrate into nvim-cmp via cmp-omni

--- lua/aj/omnisharp-vim.lua
+++ lua/aj/omnisharp-vim.lua
@@ -43,6 +43,46 @@ function M.setup()
   vim.g.OmniSharp_fzf_options = {
     down = '30%'
   }
+
+  -- setup integration with nvim-cmp and cmp-omni
+  local cmp_installed, cmp = pcall(require, 'cmp')
+  local cmp_omni_installed = pcall(require, 'cmp_omni')
+
+  if cmp_installed and cmp_omni_installed then
+    local types = require('cmp.types')
+    local omni_to_lsp_map = {
+      Parameter = 'Variable',
+      Structure = 'Struct',
+      Const = 'Constant',
+      Namespace = 'Unit',
+      Delegate = 'Class',
+      EnumMember = 'Constant'
+    }
+
+    local function normalize_kind(omni_kind)
+      omni_kind = omni_to_lsp_map[omni_kind] or omni_kind
+      local lsp_kind = types.lsp.CompletionItemKind[omni_kind] or types.lsp.CompletionItemKind.Text
+      return types.lsp.CompletionItemKind[lsp_kind]
+    end
+
+    cmp.setup.filetype('cs', {
+      sources = {
+        { name = 'omni', trigger_characters = { '.' } }
+      },
+      formatting = {
+        format = function(entry, vim_item)
+          local details = entry.completion_item.labelDetails
+          local lsp_kind = normalize_kind(details.detail)
+
+          vim_item.kind = details.detail
+          vim_item.kind_hl_group = 'CmpItemKind' .. lsp_kind
+          vim_item.menu = details.description
+
+          return vim_item
+        end
+      }
+    })
+  end
 end
 
 return M
