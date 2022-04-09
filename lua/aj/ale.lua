a9a8cbe67c1088a021e11837668ed3e1f1cf2e47
Bohdan Makohin <ajukraine@gmail.com>
2022-04-09 17:30:32 +0300

    (ale) Disable broken 'echo cursor' functionality

--- lua/aj/ale.lua
+++ lua/aj/ale.lua
@@ -7,6 +7,9 @@ function M.setup()
 
   vim.g.ale_linters_explicit = 1
 
+  -- disable ale's echo cursor, because of annoying bug when string is too long
+  vim.g.ale_echo_cursor = 0
+
   -- vim.g.ale_virtualtext_cursor = 1
 
   vim.g.ale_send_to_neovim_diagnostics = 1
