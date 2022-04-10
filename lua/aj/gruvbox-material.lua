bfc183934ecaa222f3a8d244466ccb62f524ed5d
Bohdan Makohin <ajukraine@gmail.com>
2022-04-11 02:23:21 +0300

    (gruvbox-material) Try unoptimized version

--- lua/aj/gruvbox-material.lua
+++ lua/aj/gruvbox-material.lua
@@ -1,10 +1,12 @@
 return {
   setup = function ()
-    vim.g.gruvbox_material_better_performance = 1
+    vim.g.gruvbox_material_better_performance = 0
+
+    vim.g.gruvbox_material_transparent_background = 0
+    vim.g.gruvbox_material_palette = 'material'
+    vim.g.gruvbox_material_background = 'hard'
 
     vim.g.gruvbox_italics = 0
-    vim.g.gruvbox_material_transparent_background = 0
-    vim.g.gruvbox_material_background = 'hard'
     vim.g.gruvbox_material_enable_italic = 0
-  end
+  end,
 }
