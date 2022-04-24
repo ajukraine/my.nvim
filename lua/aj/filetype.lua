52c4b9da0ff0c31924e6b4792f640e3b5234c6cf
Bohdan Makohin <ajukraine@gmail.com>
2022-04-24 16:35:53 +0300

    (filetype) Remove haxe extensions as they are now merged into plugin itself

--- lua/aj/filetype.lua
+++ lua/aj/filetype.lua
@@ -2,9 +2,6 @@ return function()
   require('filetype').setup {
     overrides = {
       extensions = {
-        -- NOTE: Required before PR is merged (https://github.com/nathom/filetype.nvim/pull/83)
-        hx   = 'haxe',
-        hxml = 'hxml',
         fs   = 'fsharp',
       }
     }
